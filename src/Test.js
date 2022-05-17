23a7d83fd01143e8685801e14803f3d5ccd37e0e
Anno100 <alessmon.acoro@googlemail.com>
2022-05-17 18:04:28 +0200

    add useable class name Test with easy parameters

--- src/Test.js
+++ src/Test.js
@@ -1,5 +1,23 @@
-class Test{
-    assertThat=()=>{
 
+
+class Test {
+    constructor(txt, TestMethod) {
+        this.txt = txt;
+        this.TestMethod = TestMethod;
+        Test.all.push(this);
     }
+    run = () => {
+        console.log(this.txt);
+    }
+    static run = () => {
+        Test.all.forEach(test => {
+            test.run();
+        });
+    }
+    static all = [];
+
+
 }
+
+module.exports.Test = Test;
+

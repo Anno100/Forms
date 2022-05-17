d0ebd0da6d750f5714652002bcba9010bf85c2d6
Anno100 <alessmon.acoro@googlemail.com>
2022-05-17 18:05:32 +0200

    test the require Method and it works

---
+++ src/test/RectTest.js
@@ -0,0 +1,12 @@
+var Test = require("../Test.js").Test;
+var Rect = require("../class/Rect.js").Rect;
+
+var test_1 = new Test("default test",()=>{
+    
+});
+var test_2 = new Test("second blabla",()=>{
+    
+});
+
+
+Test.run();
